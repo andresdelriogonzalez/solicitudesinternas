c6129ecfc955760422c6f6334e6335a6172c9728
andresdelriogonzalez <165186899+andresdelriogonzalez@users.noreply.github.com>
2025-05-26 00:11:15 +0000

    Solving error in client field

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ st.title('🎈 Solicitudes Internas')
 
 st.write('Hello Andrew!')
 
-fromuser = st.text_area("Solicitante:")
+fromuser = st.text_input("Solicitante:")
 subject = st.text_input("Asunto:")
 details = st.text_area("Detalles:")
 touser = st.text_input("Destinatario:")
-client = text_input("Cliente asociado")
+client = st.text_input("Cliente asociado")
 
