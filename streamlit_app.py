ff5261f477fbbe6272ac9b90c968ac5f65bcb867
andresdelriogonzalez <165186899+andresdelriogonzalez@users.noreply.github.com>
2025-05-26 00:07:21 +0000

    New fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('🎈 Solicitudes Internas')
 
 st.write('Hello Andrew!')
+
+fromuser = st.text_area("Solicitante:")
+subject = st.text_input("Asunto:")
+details = st.text_area("Detalles:")
+touser = st.text_input("Destinatario:")
+client = text_input("Cliente asociado")
+
